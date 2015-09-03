f70fa0e7270fb852e1183d2c0fe46ffaf5927ccc
nick-markowski <nmarkowski@keywcorp.com>
2015-09-03 16:38:44 +0000

    (SIMP-422) Remove lsb facts and make sure travis passes.
    Updated framework to match skeleton.  Replaced lsb facts.
    
    SIMP-422 #close Update sudo

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,29 +2,36 @@ require 'spec_helper'
 
 describe 'sudo' do
 
-  it { should create_class('sudo') }
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      let(:facts) do
+        facts
+      end
 
-  it do
-    should contain_concat_build('sudoers').with({
-      'order' => ['remote_sudoers', '*.alias', '*.default', '*.uspec'],
-      'target' => '/etc/sudoers',
-      #'onlyif' => Output of concat build
-    })
+      it { should create_class('sudo') }
+    
+      it do
+        should contain_concat_build('sudoers').with({
+          'order' => ['remote_sudoers', '*.alias', '*.default', '*.uspec'],
+          'target' => '/etc/sudoers',
+          #'onlyif' => Output of concat build
+        })
+      end
+    
+      it do
+        should contain_file('/etc/sudoers').with({
+          'ensure' => 'file',
+          'owner' => 'root',
+          'group' => 'root',
+          'mode' => '0440',
+          'backup' => false,
+          'audit' => 'content',
+          'subscribe' => 'Concat_build[sudoers]',
+          'require' => 'Package[sudo]'
+        })
+      end
+    
+      it { should contain_package('sudo') }
+    end
   end
-
-  it do
-    should contain_file('/etc/sudoers').with({
-      'ensure' => 'file',
-      'owner' => 'root',
-      'group' => 'root',
-      'mode' => '0440',
-      'backup' => false,
-      'audit' => 'content',
-      'subscribe' => 'Concat_build[sudoers]',
-      'require' => 'Package[sudo]'
-    })
-  end
-
-  it { should contain_package('sudo') }
-
 end
