3535b488d27fa062022c93b19c59acb4019981dc
Trevor Vaughan <tvaughan@onyxpoint.com>
2016-07-21 13:49:19 -0400

    (SIMP-1275) Validate compliance_markup mapping
    * Ported to new RPM build structure
    * Validated compliance_markup dependencies
    
    SIMP-1275 #comment validated compliance_markup mapping
    
    Change-Id: Ifb088cc86cdbeecf66f37e8fc93009c3b61092c1

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,4 +1,8 @@
 require 'beaker-rspec'
+require 'tmpdir'
+require 'yaml'
+require 'simp/beaker_helpers'
+include Simp::BeakerHelpers
 
 unless ENV['BEAKER_provision'] == 'no'
   hosts.each do |host|
@@ -11,47 +15,38 @@ unless ENV['BEAKER_provision'] == 'no'
   end
 end
 
-# returns an Array of puppet modules declared in .fixtures.yml
-def pupmods_in_fixtures_yaml
-  require 'yaml'
-  fixtures_yml = File.expand_path( '../.fixtures.yml', File.dirname(__FILE__))
-  data         = YAML.load_file( fixtures_yml )
-  repos        = data.fetch('fixtures').fetch('repositories').keys
-  symlinks     = data.fetch('fixtures').fetch('symlinks', {}).keys
-  (repos + symlinks)
-end
-
 
 RSpec.configure do |c|
-  # Project root
-  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+  # ensure that environment OS is ready on each host
+  fix_errata_on hosts
 
   # Readable test descriptions
   c.formatter = :documentation
 
   # Configure all nodes in nodeset
   c.before :suite do
-    # net-tools required for netstat utility being used by be_listening
-    if fact('osfamily') == 'RedHat' && fact('operatingsystemmajrelease') == '7'
-      pp = <<-EOS
-        package { 'net-tools': ensure => installed }
-      EOS
+    begin
+      # Install modules and dependencies from spec/fixtures/modules
+      copy_fixture_modules_to( hosts )
+      begin
+        server = only_host_with_role(hosts, 'server')
+      rescue ArgumentError =>e
+        server = only_host_with_role(hosts, 'default')
+      end
 
-      apply_manifest_on(agents, pp, :catch_failures => false)
-    end
+      # Generate and install PKI certificates on each SUT
+      Dir.mktmpdir do |cert_dir|
+        run_fake_pki_ca_on(server, hosts, cert_dir )
+        hosts.each{ |sut| copy_pki_to( sut, cert_dir, '/etc/pki/simp-testing' )}
+      end
 
-    # Install module and dependencies
-    puppet_module_install(:source => proj_root, :module_name => '<%= metadata.name %>')
-    hosts.each do |host|
-      # allow spec_prep to provide modules (to support isolated networks)
-      if ENV['BEAKER_use_fixtures_dir_for_modules'] == 'yes'
-        pupmods_in_fixtures_yaml.each do |pupmod|
-          mod_root = File.expand_path( "fixtures/modules/#{pupmod}", File.dirname(__FILE__))
-          puppet_module_install(:source => mod_root, :module_name => pupmod)
-        end
+      # add PKI keys
+      copy_keydist_to(server)
+    rescue StandardError, ScriptError => e
+      if ENV['PRY']
+        require 'pry'; binding.pry
       else
-        # TODO: update when the relevant SIMP modules are on the forge
-        on host, puppet('module', 'install', 'puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
+        raise e
       end
     end
   end
