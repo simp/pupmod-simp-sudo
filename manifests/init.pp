b9e25a6a6bb1d64a82cdef563bef01a8073c07b4
Nick Miller <nick.miller@onyxpoint.com>
2016-11-11 14:23:20 -0500

    (SIMP-1894) Create user_specification resources in Hiera
    A few new variables have been added to init.pp:
    - sudo_user_specification_hash
    - This is a data structure that allows users to create
      sudo::user_specification resources in hiera
       - See variable documentation or spec tests for examples
    - sudo_user_specification_hash_defaults
    - default type parameters to be applied to all resources
    - Added tests to make sure it works as expected
    
    SIMP-1894 #close
    SIMP-608 #comment done sudo::user_specification

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,11 +5,36 @@
 #
 # == Parameters
 #
+# [*sudo_user_specification_hash*]
+#   Default: {}
+#   Type: Hash
+#   A hash of sudo::user_specification resources that can be set in hiera
+#   Example:
+#     sudo_user_specification_hash:
+#       simp_sudosh:
+#         user_list:
+#           - simp
+#         cmnd: [ /usr/bin/sudosh ]
+#         runas: root
+#       users_yum:
+#         user_list:
+#           - '%users'
+#         cmnd:
+#           - yum
+#
+# [*sudo_user_specification_hash_defaults*]
+#   Default: {}
+#   Type: {}
+#   Defaults parameters that will get merged with the data hash at compile time
+#
 # == Authors
 #
 # * Trevor Vaughan <tvaughan@onyxpoint.com>
 #
-class sudo {
+class sudo (
+  Hash $sudo_user_specification_hash = defined('$::sudo_user_specification_hash') ? { true => $sudo_user_specification_hash, default => hiera('sudo_user_specification_hash', {}) },
+  Hash $sudo_user_specification_hash_defaults = {}
+) {
   # This builds a local 'new' sudoers file.
   $outfile = simpcat_output('sudoers')
 
@@ -33,4 +58,13 @@ class sudo {
   package {
     'sudo': ensure => 'latest'
   }
+
+  if ! empty($sudo_user_specification_hash) {
+    $sudo_user_specification_hash.each |$name, $spec| {
+      sudo::user_specification { $name:
+        * => $sudo_user_specification_hash_defaults + $spec
+      }
+    }
+  }
+
 }
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'yaml'
 
 describe 'sudo' do
 
@@ -9,29 +10,105 @@ describe 'sudo' do
       end
 
       it { should create_class('sudo') }
-    
+
       it do
         should contain_simpcat_build('sudoers').with({
-          'order' => ['remote_sudoers', '*.alias', '*.default', '*.uspec'],
+          'order'  => ['remote_sudoers', '*.alias', '*.default', '*.uspec'],
           'target' => '/etc/sudoers',
           #'onlyif' => Output of concat build
         })
       end
-    
+
       it do
         should contain_file('/etc/sudoers').with({
-          'ensure' => 'file',
-          'owner' => 'root',
-          'group' => 'root',
-          'mode' => '0440',
-          'backup' => false,
-          'audit' => 'content',
+          'ensure'    => 'file',
+          'owner'     => 'root',
+          'group'     => 'root',
+          'mode'      => '0440',
+          'backup'    => false,
+          'audit'     => 'content',
           'subscribe' => 'Simpcat_build[sudoers]',
-          'require' => 'Package[sudo]'
+          'require'   => 'Package[sudo]'
         })
       end
-    
+
       it { should contain_package('sudo') }
+
+      context 'should create sudo::user_specification resources with an iterator' do
+        context 'given something that is not a hash' do
+          let(:params) {{
+            :sudo_user_specification_hash => 'this is not a hash'
+          }}
+          it { is_expected.to raise_error }
+        end
+
+        context 'with properly formatted and complete yaml' do
+          let(:params){
+            YAML.load(<<-EOF
+sudo_user_specification_hash:
+  simp_sudosh:
+    user_list:
+      - simp
+    cmnd: [ /usr/bin/sudosh ]
+    runas: root
+  users_yum:
+    user_list:
+      - '%users'
+    cmnd:
+      - yum
+EOF
+            )
+          }
+          it { is_expected.to create_sudo__user_specification('simp_sudosh').with({
+            :user_list => ['simp'],
+            :cmnd      => ['/usr/bin/sudosh'],
+            :runas     => 'root',
+            :setenv    => true
+          })}
+          it { is_expected.to create_sudo__user_specification('users_yum').with({
+            :user_list => ['%users'],
+            :cmnd      => ['yum'],
+            :runas     => 'root',
+            :setenv    => true
+          })}
+        end
+
+        context 'with properly formatted yaml and default parameters' do
+          let(:params){
+            YAML.load(<<-EOF
+sudo_user_specification_hash:
+  simp_sudosh:
+    user_list:
+      - simp
+    cmnd: [ /usr/bin/sudosh ]
+    runas: root
+  users_yum:
+    user_list:
+      - '%users'
+    cmnd:
+      - yum
+sudo_user_specification_hash_defaults:
+  runas: puppet
+EOF
+            )
+          }
+          it { is_expected.to create_sudo__user_specification('simp_sudosh').with({
+            :user_list => ['simp'],
+            :cmnd      => ['/usr/bin/sudosh'],
+            :runas     => 'root',
+            :setenv    => true
+          })}
+          it { is_expected.to create_sudo__user_specification('users_yum').with({
+            :user_list => ['%users'],
+            :cmnd      => ['yum'],
+            :runas     => 'puppet',
+            :setenv    => true
+          })}
+        end
+
+      end
+
     end
   end
+
 end
