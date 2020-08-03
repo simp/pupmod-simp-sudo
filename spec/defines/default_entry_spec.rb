80891d57002b246e7641749628c967ee42c1e386
Mike Renfro <mike+github@renf.ro>
2020-08-03 12:04:51 -0700

    Removing (excess?) template whitespace (#54)
    * Removing (excess) whitespace from sudo template
    * Update next version to make linter happy
    * Update spec tests to match changes
    
    SIMP-7962 #close 
    
    Co-authored-by: Chris Tessmer <chris.tessmer@onyxpoint.com>

--- spec/defines/default_entry_spec.rb
+++ spec/defines/default_entry_spec.rb
@@ -14,18 +14,18 @@ describe 'sudo::default_entry' do
           it { is_expected.to compile.with_all_deps }
           it do
             is_expected.to create_concat__fragment("sudo_default_entry_#{title}")
-              .with_content("\nDefaults    first, second\n\n")
+              .with_content("Defaults    first, second\n")
           end
         end
 
         context 'def_type = host and target specified' do
-          let(:params) { {:content => ['first', 'second'], :def_type => 'host', 
+          let(:params) { {:content => ['first', 'second'], :def_type => 'host',
             :target => 'some_host_target'} }
 
           it { is_expected.to compile.with_all_deps }
           it do
             is_expected.to create_concat__fragment("sudo_default_entry_#{title}")
-              .with_content("\nDefaults@some_host_target    first, second\n\n")
+              .with_content("Defaults@some_host_target    first, second\n")
           end
         end
 
@@ -36,7 +36,7 @@ describe 'sudo::default_entry' do
           it { is_expected.to compile.with_all_deps }
           it do
             is_expected.to create_concat__fragment("sudo_default_entry_#{title}")
-              .with_content("\nDefaults!some_cmnd_target    first, second\n\n")
+              .with_content("Defaults!some_cmnd_target    first, second\n")
           end
         end
 
@@ -46,7 +46,7 @@ describe 'sudo::default_entry' do
           it { is_expected.to compile.with_all_deps }
           it do
             is_expected.to create_concat__fragment("sudo_default_entry_#{title}")
-              .with_content("\nDefaults:    first, second\n\n")
+              .with_content("Defaults:    first, second\n")
           end
         end
 
@@ -56,7 +56,7 @@ describe 'sudo::default_entry' do
           it { is_expected.to compile.with_all_deps }
           it do
             is_expected.to create_concat__fragment("sudo_default_entry_#{title}")
-              .with_content("\nDefaults>    first, second\n\n")
+              .with_content("Defaults>    first, second\n")
           end
         end
 
@@ -66,7 +66,7 @@ describe 'sudo::default_entry' do
           it { is_expected.to compile.with_all_deps }
           it do
             is_expected.to create_concat__fragment("sudo_default_entry_#{title}")
-              .with_content("\nDefaults!    first, second\n\n")
+              .with_content("Defaults!    first, second\n")
           end
         end
         # Test cve 2019-14287 mitigation
@@ -77,14 +77,14 @@ describe 'sudo::default_entry' do
             let(:facts) { os_facts.merge( { :sudo_version => '1.8.0' })}
              it do
                is_expected.to create_concat__fragment("sudo_default_entry_#{title}")
-                 .with_content("\nDefaults>    %ALL, !%wheel, !%#-1\n\n")
+                 .with_content("Defaults>    %ALL, !%wheel, !%#-1\n")
              end
           end
           context 'sudo version >= 1.8.28' do
             let(:facts) { os_facts.merge( { :sudo_version => '1.8.30' })}
              it do
                is_expected.to create_concat__fragment("sudo_default_entry_#{title}")
-                 .with_content("\nDefaults>    %ALL, !%wheel\n\n")
+                 .with_content("Defaults>    %ALL, !%wheel\n")
              end
           end
         end
