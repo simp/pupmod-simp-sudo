36a019b1710a729593a88553b2734e42628a4c86
Mike Riddle <michael.riddle@onyxpoint.com>
2021-11-29 10:39:56 -0500

    Added changelog entry and fixed syntax issue in spec test

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -67,10 +67,9 @@ describe 'sudo' do
 
         context 'should create sudo::include_dir resources with an iterator' do
           let(:hieradata) { 'sudo__include_dirs' }
-          is { is_expected.to create_sudo__include_dir('/etc/sudoers.d')
-          is { is_expected.to create_sudo__include_dir('/etc/simp_sudoers.d')
-          }
-        }
+          is { is_expected.to create_sudo__include_dir('/etc/sudoers.d') }
+          is { is_expected.to create_sudo__include_dir('/etc/simp_sudoers.d') }
+        end
       end
     end
   end
